900cba037371d245e22694bed9a87ad8b7945839
FinishedInk <finishedink6@gmail.com>
2026-08-19 20:32:29 -0400

    Enable blog feature in leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -2,7 +2,7 @@ export const leagueID = "1310420611469697024"; // your league ID
 export const leagueName = "Tha Dissy"; // your league name
 export const dues = 15; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
-export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
+export const enableBlog = true; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
 export const homepageText = `
   <p> Home Page of Tha Dissy</p>
